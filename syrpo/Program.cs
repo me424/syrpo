2bf78d1c6b61e99c4a2ecc596dce27fa3db869b8
Kamila <khuzinakamila@gmail.com>
2023-11-28 19:07:21 +0300

    'disappeared'

--- syrpo/Program.cs
+++ syrpo/Program.cs
@@ -7,6 +7,5 @@ class Program
         Console.WriteLine("Hello, World!!!!!!!!!!!!");
         Console.WriteLine("!!!!!!!!!!!!");
         Console.WriteLine('test');
-        Console.WriteLine('vabalabadabdab, Morti!');
     }
 }
